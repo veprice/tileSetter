61e185f32ae852f07c4266c086e2d6c2456d5125
Virginia <virginia.e.price@gmail.com>
2022-05-21 15:45:28 -0400

    setup for install is confusing

--- pyleset/__init__.py
+++ pyleset/__init__.py
@@ -1,5 +1,7 @@
-# import tilesetting modules from directorys
-# from .defaults import defaults
+"""
+A Python library for tilesetting Neopets.
+
+"""
 
 
 from .nameBuilder import nameGen
@@ -9,6 +11,27 @@ __version__ = '0.1.0'
 __author__ = 'Cady (penguinluver222)'
 
 def refresh(name_generator):
+    """Refreshes pet tiles with new names.
+
+        Args:
+            name_generator: a nameGen() object created with a specific string
+
+        Returns:
+            Writes updated HTML file using defined generator, and prints statistics about the names to the terminal.
+
+        Examples:
+            >>> refresh(bb)
+            ------------------- HTML Updated! ---
+            Saved to: ./HTML/tileSet.html
+            ...
+                         # of names
+            6 Letters           100
+            7 Letters           100
+            8 Letters           100
+            Total Names         300
+            -----------------------
+            Last Update @ 03:01:39 PM'
+    """
     tileSet(name_generator.get_names())
     return name_generator.show_stats()
 
--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import find_packages, setupsetup(
-    name='pyleset',
-    packages=find_packages(['pyleset']),
-    version='0.1.0',
-    description='Python library for tilesetting Neopets in the Neopian Pound',
-    long_description=open('README.md').read(),
-    author='Cady | UN: penguinluver222',
-    license='MIT',
-    url='https://github.com/veprice/tileSetter/'
-    install_requires['pandas==1.2.4','numpy']
-    include_package_data=True,
-    package_data={'': ['data/*.csv']}
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-    ]
-)
+from setuptools import setup
+
+setup()
